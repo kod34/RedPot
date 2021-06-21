a63e9ff020cdb67d427413fce9745644895f8386
New Simo <xomister1@gmail.com>
2021-06-21 19:28:15 +0100

    Update 3.6

--- redpot.sh
+++ redpot.sh
@@ -21,6 +21,9 @@ echo ‎‎‎‎'                    'Starting the SSH HoneyPot
 echo -------------------------------------------------------------------
 echo [+] Stopping SSH server
 sleep 1
+read -p 'Enter a parameter to be used as an SSH password: ' sshp
+sed -i "s/password == .*/password == '$sshp':/" /redpot/ssh/fakessh.py
+sleep 1
 echo [+] Starting Fake-SSH Honeypot...
 systemctl restart fakessh
 sleep 1
@@ -30,6 +33,11 @@ echo -------------------------------------------------------------------
 echo ‎‎‎‎'                      'Starting the RedPot IDS
 echo -------------------------------------------------------------------
 read -p 'Minimum Packet rate (Packets per minute) to be detected as a flood Attack: ' prate
+while ! [[ $prate =~ ^[0-9]+$ ]]
+do
+        echo "Error: Invalid Input"
+        read -p 'Minimum Packet rate (Packets per minute) to be detected as a flood Attack: ' prate
+done        
 sed -i "s/threshold =.*/threshold = $prate/" /redpot/IDS/src_code/intrusion.py
 sleep 1
 echo [+] Starting redpot_ids service...
